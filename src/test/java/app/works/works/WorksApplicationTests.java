dff428d2e20f621c373bedfe2b99782e5a45693d
pedrowiskutzki <wiskutzki@gmail.com>
2022-12-25 14:24:42 -0300

    feat(Init):Init Api

---
+++ src/test/java/app/works/works/WorksApplicationTests.java
@@ -0,0 +1,13 @@
+package app.works.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
